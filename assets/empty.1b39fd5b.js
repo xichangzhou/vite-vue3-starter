1254809df389b39f929b813beab07dc1ae55e2e4
xichangzhou <xichangzhou@users.noreply.github.com>
2021-05-22 06:10:23 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter dde09cc4b4f005e30e3d82129dccf36a273df2d8

---
+++ assets/empty.1b39fd5b.js
@@ -0,0 +1,1 @@
+import{o as r,j as e}from"./vendor.678033df.js";const n={};n.render=function(n,o,t,d,f,s){return r(),e("div",null,"empty")};export default n;
---
+++ assets/profile.674a0166.js
@@ -0,0 +1,1 @@
+import{o as r,j as o}from"./vendor.678033df.js";const e={};e.render=function(e,n,t,d,f,i){return r(),o("div",null,"Profile")};export default e;
