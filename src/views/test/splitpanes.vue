14ac2db228188e9233264a650ed4663fdd4da8c1
jsonxcz <jsonxcz@QQ.COM>
2021-05-24 14:22:41 +0800

    feat: vue3-carousel

--- src/views/test/splitpanes.vue
+++ src/views/test/splitpanes.vue
@@ -4,7 +4,18 @@
             <div style="padding: 30px"><span>Contextual Navigation</span></div>
         </pane>
         <pane size="80">
-            <div style="padding: 30px"><span>Editor</span></div>
+            <div style="padding: 30px">
+                <span
+                    ><Carousel :items-to-show="2.5" :wrap-around="true">
+                        <Slide v-for="slide in 10" :key="slide">
+                            <div class="carousel__item">{{ slide }}</div>
+                        </Slide>
+
+                        <template #addons>
+                            <Navigation />
+                        </template> </Carousel
+                ></span>
+            </div>
         </pane>
     </splitpanes>
 </template>
@@ -13,14 +24,42 @@
 import { defineComponent } from 'vue'
 import { Splitpanes, Pane } from 'splitpanes'
 
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Navigation } from 'vue3-carousel'
+
 export default defineComponent({
     name: 'SplitPanes',
     components: {
         Splitpanes,
-        Pane
+        Pane,
+        Carousel,
+        Slide,
+        Navigation
     },
     setup() {}
 })
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.carousel__item {
+    min-height: 200px;
+    width: 100%;
+    background-color: var(--carousel-color-primary);
+    color: var(--carousel-color-white);
+    font-size: 20px;
+    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.carousel__slide {
+    padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+    box-sizing: content-box;
+    border: 5px solid white;
+}
+</style>
