35807ac70b6e3458d724afaad5ab7acc92f804c7
jsonxcz <jsonxcz@QQ.COM>
2021-05-24 16:46:35 +0800

    feat: xvirtualList

--- src/views/test/hooks.vue
+++ src/views/test/hooks.vue
@@ -2,15 +2,28 @@
     <input v-model="title" />
     <button @click="updateTitle">更新标题</button>
     <v-md-editor v-model="text" height="400px"></v-md-editor>
+    <div class="list-container">
+        <XVirtualList :data="dataSource">
+            <template v-slot="{ item, index }">
+                <div class="item-container">
+                    <div class="cell cell-index">{{ index + 1 }}</div>
+                    <div class="cell">{{ item.name }}</div>
+                    <div class="cell">{{ item.email }}</div>
+                    <div class="cell">{{ item.address }}</div>
+                </div>
+            </template>
+        </XVirtualList>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import useTitle from '@/hooks/useTitle'
+import XVirtualList from '@/components/XVirtualList.vue'
 
 export default defineComponent({
     name: 'Hooks',
-    components: {},
+    components: { XVirtualList },
     setup() {
         const setTitle = useTitle('title', true)
         const title = ref('title')
@@ -21,7 +34,14 @@ export default defineComponent({
         return {
             title,
             updateTitle,
-            text
+            text,
+            dataSource: [
+                { name: '1', email: 'a', address: 'a' },
+                { name: '1', email: 'a', address: 'a' },
+                { name: '1', email: 'a', address: 'a' },
+                { name: '1', email: 'a', address: 'a' },
+                { name: '1', email: 'a', address: 'a' }
+            ]
         }
     }
 })
@@ -48,5 +68,31 @@ export default defineComponent({
 .carousel__next {
     box-sizing: content-box;
     border: 5px solid white;
+}
+
+.list-container {
+    width: 100%;
+    height: 600px;
+    border: 2px solid #4caf50;
+    margin: 20px auto;
+}
+.item-container {
+    height: 40px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    border-bottom: 1px solid #ccc;
+}
+.cell {
+    margin: 0 8px;
+    flex: 1;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    text-align: left;
+    white-space: nowrap;
+    &.cell-index {
+        flex: none;
+        min-width: 60px;
+    }
 }
 </style>
