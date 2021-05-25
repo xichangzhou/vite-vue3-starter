dee751bda432453cf4318ad63b3ed45ef4782b49
jsonxcz <jsonxcz@QQ.COM>
2021-05-25 14:26:20 +0800

    feat: xdropdown组件

--- src/views/test/profile.vue
+++ src/views/test/profile.vue
@@ -1,9 +1,22 @@
 <template>
     <div>Profile</div>
+    <XDropdown :title="`hello `">
+        <x-dropdown-item>
+            <router-link :to="`/home`" class="dropdown-item">新建文章</router-link>
+        </x-dropdown-item>
+    </XDropdown>
 </template>
 
 <script>
-export default {}
+import { defineComponent } from 'vue'
+import XDropdown from '@/components/XDropdown/XDropdown.vue'
+import XDropdownItem from '@/components/XDropdown/XDropdownItem.vue'
+
+export default defineComponent({
+    name: 'Profiles',
+    components: { XDropdown, XDropdownItem },
+    setup() {}
+})
 </script>
 
 <style></style>
